11dd120386b048d1e7ce1647d6d9d44b44cb2400
Taylor <meisterttl@gmail.com>
2024-06-13 18:34:03 +0000

    Working on exercises

--- 03_02b/script.js
+++ 03_02b/script.js
@@ -1,5 +1,9 @@
 class UniqueClass {
   constructor(data) {
+    if (UniqueClass.instance) {
+      return UniqueClass.instance;
+    }
+    UniqueClass.instance = this;
     this.data = data;
   }
 
@@ -11,6 +15,9 @@ class UniqueClass {
 const instance1 = new UniqueClass(
   "Data for the first instance of the UniqueClass."
 );
+
+Object.freeze(instance1);
+instance1.data = "Something else";
 
 const instance2 = new UniqueClass(
   "The second instance of the UniqueClass should not be set."
